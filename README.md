ac1f423130c70c6803b9de2252be120f7b8dfa42
Marc Wäckerlin <marc@mrw.dev>
2021-06-26 15:39:40 +0200

    get ssl certificate from Let's Encrypt

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dovecot
+# Dovecot Docker Image
+
 Docker Image for Dovecot IMAP Server
+
+Set your domain to variable `DOMAIN` and mount `/etc/letsencrypt` from your Let's Encrypt volume to use correctly configured SSL.
--- start.sh
+++ start.sh
@@ -8,4 +8,14 @@ default_pass_scheme =  SHA512-CRYPT
 EOF
 chmod 600 /etc/dovecot/dovecot-sql.conf
 
+if test -e /etc/letsencrypt/live/${DOMAIN}/fullchain.pem \
+    -a -e /etc/letsencrypt/live/${DOMAIN}/privkey.pem; then
+    cat <<EOF >/etc/dovecot/conf.d/10-ssl.conf
+ssl = required
+ssl_cert = </etc/letsencrypt/live/${DOMAIN}/fullchain.pem
+ssl_key = </etc/letsencrypt/live/${DOMAIN}/privkey.pem
+ssl_prefer_server_ciphers = yes
+EOF
+fi
+
 dovecot -F
